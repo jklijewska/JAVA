427e138d662598f11e48f31b1432d2913bf823f6
jklijewska <jklijewska@gmail.com>
2017-12-08 18:09:19 +0100

    Initial commit for quiz repo

---
+++ Quiz/src/test/java/klijewska/justyna/Quiz/QuizApplicationTests.java
@@ -0,0 +1,16 @@
+package klijewska.justyna.Quiz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
